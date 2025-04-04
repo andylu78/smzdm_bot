035682458428ea04981919cea3c341ba845def08
andylu78 <130451232+andylu78@users.noreply.github.com>
2025-04-04 18:41:31 +0800

    Update config.py

--- config.py
+++ config.py
@@ -18,7 +18,8 @@ DEFAULT_HEADERS = {
 """
 调试用 COOKIE
 """
-TEST_COOKIE = ''
+TEST_COOKIE = '__ckguid=AqQ4yv5gatbhw18t5TPoxH; device_id=213070643317437626176064902880bcba9fa547810250d719dd2c12fd; homepage_sug=d; r_sort_type=score; _zdmA.vid=*; Hm_lvt_9b7ac3d38f30fe89ff0b8a0546904e58=1743762619; HMACCOUNT=120BC63725EC9F2E; footer_floating_layer=0; ad_date=4; ad_json_feed=%7B%7D; sajssdk_2015_cross_new_user=1; sess=BA-hWixfwjW2xb79E8SaX1xnP5SbnwEW8WyaIzSSkUmBBYvmYkwIblOy4XlrX8S3W7BwWEuxv73BZjdwCEac6I9us76jDDOGn312vRQ5v%2BzDOLTIhBH7zu%2FbnNJ; user=qq_ua1q2%7C8226999044; smzdm_id=8226999044; _zdmA.uid=ZDMA.ZK-uoxeaZP.1743762636.2419200; sensorsdata2015jssdkcross=%7B%22distinct_id%22%3A%228226999044%22%2C%22first_id%22%3A%221960059fb5cde6-0a3171b521a4468-26011d51-1327104-1960059fb5d1182%22%2C%22props%22%3A%7B%22%24latest_traffic_source_type%22%3A%22%E7%9B%B4%E6%8E%A5%E6%B5%81%E9%87%8F%22%2C%22%24latest_search_keyword%22%3A%22%E6%9C%AA%E5%8F%96%E5%88%B0%E5%80%BC_%E7%9B%B4%E6%8E%A5%E6%89%93%E5%BC%80%22%2C%22%24latest_referrer%22%3A%22%22%2C%22%24latest_landing_page%22%3A%22https%3A%2F%2Fwww.smzdm.com%2F%22%7D%2C%22identities%22%3A%22eyIkaWRlbnRpdHlfY29va2llX2lkIjoiMTk2MDA1OWZiNWNkZTYtMGEzMTcxYjUyMWE0NDY4LTI2MDExZDUxLTEzMjcxMDQtMTk2MDA1OWZiNWQxMTgyIiwiJGlkZW50aXR5X2xvZ2luX2lkIjoiODIyNjk5OTA0NCJ9%22%2C%22history_login_id%22%3A%7B%22name%22%3A%22%24identity_login_id%22%2C%22value%22%3A%228226999044%22%7D%2C%22%24device_id%22%3A%221960059fb5cde6-0a3171b521a4468-26011d51-1327104-1960059fb5d1182%22%7D; Hm_lpvt_9b7ac3d38f30fe89ff0b8a0546904e58=1743762636; bannerCounter=%5B%7B%22number%22%3A1%2C%22surplus%22%3A1%7D%2C%7B%22number%22%3A0%2C%22surplus%22%3A1%7D%2C%7B%22number%22%3A0%2C%22surplus%22%3A1%7D%2C%7B%22number%22%3A0%2C%22surplus%22%3A1%7D%2C%7B%22number%22%3A1%2C%22surplus%22%3A1%7D%5D; _zdmA.time=1743762636283.0.https%3A%2F%2Fwww.smzdm.com%2F
+'
 
 
 """
